6bdc0fe02bee07ad0299fbc7871f75437866843a
Alborz Babaie Mouziraji <alborz.babaie@gmail.com>
2020-08-14 00:11:48 +0200

    Removed most of the norm errors.

--- libft/ft_wordcount.c
+++ libft/ft_wordcount.c
@@ -1,19 +1,30 @@
-int ft_wordcount(char const *str, char c){
-    int count;
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_wordcount.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababaie- <ababaie-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/13 23:54:47 by ababaie-          #+#    #+#             */
+/*   Updated: 2020/08/13 23:57:24 by ababaie-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    count = 0;
-    i = 0;
-    while(str[i] != '\0'){
-        while(str[i] == c){
-            i++;
-        }
-        if(str[i] != c  && str[i] != '\0'){
-            count++;
-        }
-        while(str[i] != c  && str[i] != '\0'){
-            i++;
-        }
-    }
-    return count;
+int	ft_wordcount(char const *str, char c)
+{
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != c && str[i] != '\0')
+			count++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+	}
+	return (count);
 }
