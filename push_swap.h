a29c6816edfbabd6b3b6869b0c71b2a049d07737
Alborz Babaie Mouziraji <alborz.babaie@gmail.com>
2020-09-10 21:37:22 +0200

    Resolving global scope error.

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ababaie- <ababaie-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 19:30:00 by ababaie-          #+#    #+#             */
-/*   Updated: 2020/09/10 15:59:28 by ababaie-         ###   ########.fr       */
+/*   Updated: 2020/09/10 21:23:40 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,53 +31,53 @@
 
 typedef	struct s_stack	t_stack;
 
-struct					s_stack
+struct			s_stack
 {
-	int					num;
-	t_stack				*next;
+	int			num;
+	t_stack		*next;
 };
 
-t_stack					*new_node(int val);
-t_stack					*append(t_stack *end, t_stack *new);
-void					print_stack(t_stack *start);
-void					print_stacks(t_stack *stack_a, t_stack *stack_b);
-void					free_stack(t_stack *start);
-void					sort_3(t_stack **stack_a, t_stack **stack_b);
-void					make_stack(int ac, char **av, t_stack **start);
-void					print_do_op(char *s, t_stack **stack_a,
+t_stack			*new_node(int val);
+t_stack			*append(t_stack *end, t_stack *new);
+void			print_stack(t_stack *start);
+void			print_stacks(t_stack *stack_a, t_stack *stack_b);
+void			free_stack(t_stack *start);
+void			sort_3(t_stack **stack_a, t_stack **stack_b);
+void			make_stack(int ac, char **av, t_stack **start);
+void			print_do_op(char *s, t_stack **stack_a,
 							t_stack **stack_b);
-void					do_op(char *line, t_stack **stack_a, t_stack **stack_b);
-void					op_sa(t_stack **start);
-void					op_ra(t_stack **start);
-void					op_rra(t_stack **start);
-void					op_pb(t_stack **stack_a, t_stack **stack_b);
-void					swap(t_stack **x, t_stack **y);
-int						stack_len(t_stack **head);
-int						found_dupes(int ac, char **av);
-int						is_sorted(t_stack *start);
-int						validate_input(int ac, char **av);
+void			do_op(char *line, t_stack **stack_a, t_stack **stack_b);
+void			op_sa(t_stack **start);
+void			op_ra(t_stack **start);
+void			op_rra(t_stack **start);
+void			op_pb(t_stack **stack_a, t_stack **stack_b);
+void			swap(t_stack **x, t_stack **y);
+int				stack_len(t_stack **head);
+int				found_dupes(int ac, char **av);
+int				is_sorted(t_stack *start);
+int				validate_input(int ac, char **av);
 
-int						find_min(t_stack *stack);
-int						find_max(t_stack *stack);
-void					push_min_or_max_b(t_stack **a, t_stack **b, int repeat,
+int				find_min(t_stack *stack);
+int				find_max(t_stack *stack);
+void			push_min_or_max_b(t_stack **a, t_stack **b, int repeat,
 							int a_len);
-void					push_max_a(t_stack **b, t_stack **a, int repeat);
-int						find_position(t_stack *start, int n);
+void			push_max_a(t_stack **b, t_stack **a, int repeat);
+int				find_position(t_stack *start, int n);
 
-void					sort_4_or_5(t_stack **a, t_stack **b);
+void			sort_4_or_5(t_stack **a, t_stack **b);
 
-int						valid_input_string(char **av);
-int						valid_input_int(int ac, char **av);
-int						only_digits(char *str);
-int						int_overflows(const char *str);
-int						valid_input(int ac, char **av);
-int						check_op(char *line);
+int				valid_input_string(char **av);
+int				valid_input_int(int ac, char **av);
+int				only_digits(char *str);
+int				int_overflows(const char *str);
+int				valid_input(int ac, char **av);
+int				check_op(char *line);
 
-void					sort(t_stack **a, t_stack **b, int len);
-void					sort_20(t_stack **a, t_stack **b, int len);
-void					push_max_a_r(t_stack **b, t_stack **a, int repeat);
-int						find_min_or_max(t_stack *stack, int type);
-int						find_position_r(t_stack *stack, int value);
-int						list_length(t_stack **head);
-int						contains_range(t_stack *stack, int range);
+void			sort(t_stack **a, t_stack **b, int len);
+void			sort_20(t_stack **a, t_stack **b, int len);
+void			push_max_a_r(t_stack **b, t_stack **a, int repeat);
+int				find_min_or_max(t_stack *stack, int type);
+int				find_position_r(t_stack *stack, int value);
+int				list_length(t_stack **head);
+int				contains_range(t_stack *stack, int range);
 #endif
