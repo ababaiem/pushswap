6f6f2ac74f1a2db8c2431046883c2920ba03c03d
Alborz Babaie Mouziraji <alborz.babaie@gmail.com>
2020-09-10 15:54:40 +0200

    Resolved norm error in push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -6,25 +6,28 @@
 /*   By: ababaie- <ababaie-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 19:35:09 by ababaie-          #+#    #+#             */
-/*   Updated: 2020/08/14 00:09:07 by alborz           ###   ########.fr       */
+/*   Updated: 2020/09/10 15:53:46 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
-# include <stdlib.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/uio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
-int   main(int ac, char **av)
+int	main(int ac, char **av)
 {
+	t_stack *stack_a;
+	t_stack *stack_b;
+
 	if (ac < 3)
 		return (0);
 	if (validate_input(ac, av))
 	{
-		t_stack *stack_a = NULL;
-		t_stack *stack_b = NULL;
+		stack_a = NULL;
+		stack_b = NULL;
 		make_stack(ac, av, &stack_a);
 		sort(&stack_a, &stack_b, ac - 1);
 		free_stack(stack_a);
