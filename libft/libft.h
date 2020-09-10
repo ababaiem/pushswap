846e75da2bb6edfa026720406dbb174fb6ff548c
Alborz Babaie Mouziraji <alborz.babaie@gmail.com>
2020-09-10 23:04:34 +0200

    Resolved all the norm errors with libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ababaie- <ababaie-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 18:31:05 by ababaie-          #+#    #+#             */
-/*   Updated: 2020/09/10 21:35:27 by ababaie-         ###   ########.fr       */
+/*   Updated: 2020/09/10 23:02:03 by ababaie-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ struct			s_list
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memccpy(void *dst, const void *src, int c,
-		size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, \
+					size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
-int				ft_memcmp(const void *s1, const void *s2,
-		size_t n);
+int				ft_memcmp(const void *s1, const void *s2, \
+						size_t n);
 char			*ft_strdup(const char *s1);
 char			*ft_strcpy(char *dst, const char *src);
 char			*ft_strncpy(char *dst, const char *src, size_t len);
@@ -46,11 +46,11 @@ size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strstr(const char *haystack, const char *needle);
-char			*ft_strnstr(const char *haystack, const char *needle,
-		size_t len);
+char			*ft_strnstr(const char *haystack, const char *needle, \
+					size_t len);
 int				ft_strcmp(const char *s1, const char *s2);
-int				ft_strncmp(const char *s1, const char *s2,
-		size_t n);
+int				ft_strncmp(const char *s1, const char *s2, \
+						size_t n);
 int				ft_atoi(const char *str);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
@@ -68,13 +68,13 @@ void			ft_strclr(char *s);
 void			ft_striter(char *s, void (*f)(char *));
 void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 char			*ft_strmap(char const *s, char (*f)(char));
-char			*ft_strmapi(char const *s,
-		char (*f)(unsigned int, char));
+char			*ft_strmapi(char const *s, \
+					char (*f)(unsigned int, char));
 int				ft_strequ(char const *s1, char const *s2);
-int				ft_strnequ(char const *s1, char const *s2,
-		size_t n);
-char			*ft_strsub(char const *s, unsigned int start,
-		size_t len);
+int				ft_strnequ(char const *s1, char const *s2, \
+						size_t n);
+char			*ft_strsub(char const *s, unsigned int start, \
+					size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s);
 char			**ft_strsplit(char const *s, char c);
@@ -93,11 +93,10 @@ int				ft_wordcount(char const *str, char c);
 int				ft_islower(int c);
 int				ft_isupper(int c);
 int				ft_iswhitespace(int c);
-t_list				*ft_lstnew(void const *content,
-		size_t content_size);
+t_list			*ft_lstnew(void const *content, size_t content_size);
 
-void			ft_lstdelone(t_list **alst,
-		void (*del)(void *, size_t));
+void			ft_lstdelone(t_list **alst, \
+					void (*del)(void *, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
